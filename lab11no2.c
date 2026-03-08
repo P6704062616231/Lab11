be73bd71f998bda87157d9387692163455a31ddb
6704062616231 <s6704062616231@email.kmutnb.ac.th>
2026-03-09 00:05:49 +0700

    Add files via upload

--- lab11no2.c
+++ lab11no2.c
@@ -1,65 +1,117 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define NO_SCORE 5
+#define NO_STUDENTS 10
 typedef struct {
-    char name[50];
-    int score;
+    char name[16];
+    char surname[20];
+    int score[NO_SCORE];
+    float total;
 } Student;
 
+void readStudentData(Student *);
+void findTotalScore(Student *);
+float findAverage(Student);
+void findLessThanTen(Student);
+void saveToFile(Student students[], int n);
+void readFromFile(Student students[], int n);
+void printStudentData(Student students[], int n);
+
 int main() {
-    Student std[10];
-    FILE *fp;
-    int i, sum = 0;
+    Student students[NO_STUDENTS];
     float avg;
-    int minIndex = 0;
+    int i;
 
-    for(i = 0; i < 10; i++) {
-        printf("Student %d name: ", i + 1);
-        scanf("%s", std[i].name);
-        printf("Student %d score: ", i + 1);
-        scanf("%d", &std[i].score);
-    }
-    fp = fopen("./lab11/no2/std10.dat", "wb");
-    if(fp == NULL) {
-        printf("Cannot open file\n");
-        return 1;
-    }
-    fwrite(std, sizeof(Student), 10, fp);
-    fclose(fp);
 
-    printf("\nSave file complete already\n");
+    for (i = 0; i < NO_STUDENTS; i++) {
+        printf("\nEnter data for student %d:\n", i + 1);
+        readStudentData(&students[i]);
+        findTotalScore(&students[i]);
+    }
 
-    fp = fopen("./lab11/no2/std10.dat", "rb");
-    if(fp == NULL) {
-        printf("Cannot open file\n");
-        return 1;
-    }
-    fread(std, sizeof(Student), 10, fp);
-    fclose(fp);
+    saveToFile(students, NO_STUDENTS);
 
-    for(i = 0; i < 10; i++) {
-        sum += std[i].score;
-    }
-    avg = (float)sum / 10;
+    readFromFile(students, NO_STUDENTS);
 
-    for(i = 1; i < 10; i++) {
-        if(std[i].score < std[minIndex].score) {
-            minIndex = i;
-        }
-    }
-    printf("\n--- Student Records ---\n");
-    for(i = 0; i < 10; i++) {
-        printf("Name: %s, Score: %d\n", std[i].name, std[i].score);
-    }
-    printf("\nAverage score = %.2f\n", avg);
+    printStudentData(students, NO_STUDENTS);
 
-    printf("Student who got less than 10 score:\n");
-    for(i = 0; i < 10; i++) {
-        if(std[i].score < 10) {
-            printf("Name: %s, Score: %d\n", std[i].name, std[i].score);
-        }
+    for (i = 0; i < NO_STUDENTS; i++) {
+        avg = findAverage(students[i]);
+        printf("\nAverage score for student %d (%s %s) is %.2f", i + 1, students[i].name, students[i].surname, avg);
     }
-    printf("\nLowest score student: %s, Score: %d\n",
-           std[minIndex].name, std[minIndex].score);
+
+    for (i = 0; i < NO_STUDENTS; i++) {
+        printf("\n\nChecking scores less than 10 for student %d (%s %s):", i + 1, students[i].name, students[i].surname);
+        findLessThanTen(students[i]);
+    }
 
     return 0;
+}
+void readStudentData(Student *pStd) {
+    int i;
+    printf("\tName : ");
+    scanf("%s", pStd->name);
+    printf("\tSurname : ");
+    scanf("%s", pStd->surname);
+    for (i = 0; i < NO_SCORE; i++) {
+        printf("\tScore %d : ", i + 1);
+        scanf("%d", &pStd->score[i]);
+    }
+}
+void findTotalScore(Student *pStd) {
+    int i;
+    pStd->total = 0.0;
+    for (i = 0; i < NO_SCORE; i++) {
+        pStd->total += pStd->score[i];
+    }
+}
+
+float findAverage(Student s) {
+    return s.total / NO_SCORE;
+}
+void findLessThanTen(Student s) {
+    int i, count = 0;
+    for (i = 0; i < NO_SCORE; i++) {
+        if (s.score[i] < 10) {
+            printf("\n\tTest no.%d - %d", i + 1, s.score[i]);
+            count++;
+        }
+    }
+    if (count == 0) {
+        printf(" -> None");
+    }
+}
+void saveToFile(Student students[], int n) {
+    FILE *file;
+    file = fopen("./lab11/no2/std10.dat", "wb");
+    if (file == NULL) {
+        printf("Error opening file for writing!\n");
+        exit(1);
+    }
+    fwrite(students, sizeof(Student), n, file);
+    fclose(file);
+    printf("\nData saved to file successfully!\n");
+}
+void readFromFile(Student students[], int n) {
+    FILE *file;
+    file = fopen("./lab11/no2/std10.dat", "rb");
+    if (file == NULL) {
+        printf("Error opening file for reading!\n");
+        exit(1);
+    }
+    fread(students, sizeof(Student), n, file);
+    fclose(file);
+    printf("\nData read from file successfully!\n");
+}
+void printStudentData(Student students[], int n) {
+    int i, j;
+    printf("\n\nStudent Data:\n");
+    for (i = 0; i < n; i++) {
+        printf("\nStudent %d: %s %s\n", i + 1, students[i].name, students[i].surname);
+        printf("Scores: ");
+        for (j = 0; j < NO_SCORE; j++) {
+            printf("%d ", students[i].score[j]);
+        }
+        printf("\nTotal Score: %.2f", students[i].total);
+    }
 }
